a69e1e280c09202354d93435ab76b5a8730005af
Frank Wei <frank@frankwei.xyz>
2020-10-29 15:15:28 +0800

    renaming

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.bscgraph.org/subgraphs/name/teambest/bestswap',
+    uri: 'https://api.bscgraph.org/subgraphs/name/uniscam/swap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
