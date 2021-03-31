35582dbf639a1428a7eba784160b0d6389a101b3
神樹桜乃 <kodama@sakuno.moe>
2021-03-31 16:11:34 +0800

    Update TokenData.js

--- src/contexts/TokenData.js
+++ src/contexts/TokenData.js
@@ -286,9 +286,9 @@ const getTopTokens = async (ethPrice, ethPriceOld) => {
             data.oneDayTxns = data.txCount
           }
 
-          if (data.id === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') {
-            data.name = 'Ether (Wrapped)'
-            data.symbol = 'ETH'
+          if (data.id === '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c') {
+            data.name = 'BNB (Wrapped)'
+            data.symbol = 'BNB'
           }
           return data
         })
